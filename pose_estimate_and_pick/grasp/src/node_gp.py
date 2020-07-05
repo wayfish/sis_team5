8e8deacf2b7b1e0b259f4592d6e48465f2aea727
wayfish <61940291+wayfish@users.noreply.github.com>
2020-07-05 22:18:25 +0800

    grasp

--- pose_estimate_and_pick/grasp/src/node_gp.py
+++ pose_estimate_and_pick/grasp/src/node_gp.py
@@ -14,9 +14,9 @@ pose_pub = rospy.Publisher("ob_pose",PoseStamped,queue_size=1)
 ob_pose = Pose()
 ob_ps = PoseStamped()
 
-ob_pose.position.x = 0.32 - 0.03
-ob_pose.position.y = 0.0 - 0.02
-ob_pose.position.z = 0.052 -0.01 #0.14  
+ob_pose.position.x = 0.35
+ob_pose.position.y = 0.0
+ob_pose.position.z = 0.06 #0.14  
 ob_pose.orientation.x = 0
 ob_pose.orientation.y = 0.707
 ob_pose.orientation.z = 0
@@ -24,7 +24,7 @@ ob_pose.orientation.w = 0.707
 
 ob_ps.pose.position.x = 0.35
 ob_ps.pose.position.y = 0.0
-ob_ps.pose.position.z = 0.052  #0.14
+ob_ps.pose.position.z = 0.06 #0.14
 ob_ps.pose.orientation.x = 0
 ob_ps.pose.orientation.y = 0
 ob_ps.pose.orientation.z = 0
@@ -39,7 +39,7 @@ rospy.init_node('gp_node')
 r = rospy.Rate(10)
 
 counter = 0
-while counter<1000:
+while counter<300:
     pose_pub.publish(deck_ps)
     r.sleep()
     if counter%100 ==0:
